40f5fe4c483c0f46bfa72500abfc450583704eb0
CryKitty <raayve@gmail.com>
2023-05-16 13:03:18 -0500

    Update list.js

--- list.js
+++ list.js
@@ -336,38 +336,42 @@ document.querySelectorAll('.card-number').forEach((element) => {
   }
 });
 
-// Add event listeners to the delete button
-const deleteButton = document.querySelector("#delete-button");
-deleteButton.addEventListener('dragover', deleteOver);
-deleteButton.addEventListener('dragenter', deleteEnter);
-deleteButton.addEventListener('dragleave', deleteLeave);
-deleteButton.addEventListener('drop', deleteDrop);
+// Select all delete buttons
+const deleteButtons = document.querySelectorAll(".delete-button");
 
-// When a card is over the delete button
-function deleteOver(e) {
-  e.preventDefault();
-  deleteButton.style.backgroundColor = 'red'; // Change the color of the delete button to indicate it's active
-}
+// Iterate over each delete button and assign event listeners
+deleteButtons.forEach(deleteButton => {
+  deleteButton.addEventListener('dragover', deleteOver);
+  deleteButton.addEventListener('dragenter', deleteEnter);
+  deleteButton.addEventListener('dragleave', deleteLeave);
+  deleteButton.addEventListener('drop', deleteDrop);
 
-// When a card enters the delete button area
-function deleteEnter(e) {
-  e.preventDefault();
-}
-
-// When a card leaves the delete button area
-function deleteLeave(e) {
-  deleteButton.style.backgroundColor = ''; // Reset the color of the delete button
-}
-
-// When a card is dropped on the delete button
-function deleteDrop(e) {
-  e.preventDefault();
-  if (draggedItem != null) {
-    draggedItem.remove();
-    draggedItem = null;
+  // When a card is over the delete button
+  function deleteOver(e) {
+    e.preventDefault();
+    deleteButton.style.backgroundColor = 'red'; // Change the color of the delete button to indicate it's active
   }
-  deleteButton.style.backgroundColor = ''; // Reset the color of the delete button
-}
+
+  // When a card enters the delete button area
+  function deleteEnter(e) {
+    e.preventDefault();
+  }
+
+  // When a card leaves the delete button area
+  function deleteLeave(e) {
+    deleteButton.style.backgroundColor = ''; // Reset the color of the delete button
+  }
+
+  // When a card is dropped on the delete button
+  function deleteDrop(e) {
+    e.preventDefault();
+    if (draggedItem != null) {
+      draggedItem.remove();
+      draggedItem = null;
+    }
+    deleteButton.style.backgroundColor = ''; // Reset the color of the delete button
+  }
+});
 
 // Dark Mode
 document.addEventListener('DOMContentLoaded', (event) => {
